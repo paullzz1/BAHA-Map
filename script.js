03f64a956e89a11789fceed029db05fa5dfb7fde
paullzz1 <paull@berkeley.edu>
2025-10-30 17:25:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bGx6eiIsImEiOiJjbHNueGMxYWgwOHpvMmtuemEydHNmeDV4In0.JetyXo65-dGtsabt-mujBA';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/paullzz/cmh9rkbjt00qn01sm0okrciab',
+  center: [-123.047, 49.256], 
+  zoom: 9 // starting zoom
+    });
